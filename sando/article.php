bd0522341584c49cbe29a3ba51841b9659b2b1c0
DavidDarnes <me@daviddarnes.com>
2013-01-12 17:53:46 +0000

    Fixed sando. All done!

--- sando/article.php
+++ sando/article.php
@@ -1,3 +1,4 @@
+<?php theme_include('header'); ?>
 <article>
     <h2 id="postTitle"><?php echo article_title(); ?></h2>
     <p class="postMeta">
@@ -7,12 +8,13 @@
             <span>&rarr;</span> <a href="<?php echo admin_url(); ?>/posts/edit/<?php echo article_id(); ?>">Edit</a>
         <?php endif; ?>
     </p>
-    
-    
+
+
     <div class="content">
         <?php echo article_html(); ?>
     </div>
-    
-    
+
+
     <p class="postFooter"><p>This article is my <?php echo numeral(article_id() + 1); ?> oldest. It is <?php echo count_words(article_html()); ?> words long.</p>
 </article>
+<?php theme_include('footer'); ?>
